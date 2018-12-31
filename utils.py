f5c26afac2c4eee6cdca2121202fb77330b95568
Chen Yufei <yufei.chen@pku.edu.cn>
2018-12-31 03:13:41 +0000

    remove unused function

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import torch
+from torch import Tensor
 
 
 def convert_to_torch_tensor(inputs, device=None):
     for i in inputs.keys():
+        if isinstance(inputs[i], Tensor):
+            inputs[i] = inputs[i].to(device)
         inputs[i] = torch.tensor(inputs[i], device=device)
 
-
-def smart_from_numpy(ndarray, device=torch.device("cpu")):
-    return torch.from_numpy(ndarray).pin_memory().to(device)
